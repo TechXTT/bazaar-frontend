4a00632e1f90f67abee46a42936ee8389ebf246c
Martin Bozhilov <martin@bozhilov.me>
2023-11-20 08:51:09 +0200

    [INIT] Project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { TbClockHour4, TbMapPin } from 'react-icons/tb';
+
+import 'animate.css';
+
+export default function Home() {
+	return (
+		<main className="max-w-screen-2xl m-auto">
+		</main>
+	);
+}
