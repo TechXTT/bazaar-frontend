06ff9490a4c42d970a8533a66ea388fdeddf84b8
Martin Bozhilov <martin@bozhilov.me>
2024-02-27 22:13:49 +0200

    [FIX] location href

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import Navigation from "./components/navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import ReduxProvider from "./components/redux";
-import { MetaMaskProvider } from '@metamask/sdk-react';
+import { MetaMaskProvider } from "@metamask/sdk-react";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,35 +19,43 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [location, setLocation] = useState<string>("");
+  useEffect(() => {
+    setLocation(window.location.href);
+  }, []);
 
   return (
     <html lang="en">
       <head>
-				<Script src="https://www.googletagmanager.com/gtag/js?id=G-1H1H1CR559" strategy="afterInteractive" />
-				<Script id="google-analytics" strategy="afterInteractive">
-					{`
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-1H1H1CR559"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
 						window.dataLayer = window.dataLayer || [];
 						function gtag(){dataLayer.push(arguments);}
 						gtag('js', new Date());
 
 						gtag('config', 'G-1H1H1CR559');
 					`}
-				</Script>
-			</head>
+        </Script>
+      </head>
       <body className="bg-[#182628]">
-      <ReduxProvider>
-        <MetaMaskProvider debug={true} sdkOptions={{
-          dappMetadata: {
-            name: 'The Bazaar',
-            url: window.location.href,
-          }
-        }}>
+        <ReduxProvider>
+          <MetaMaskProvider
+            debug={true}
+            sdkOptions={{
+              dappMetadata: {
+                name: "The Bazaar",
+                url: location,
+              },
+            }}
+          >
             <Navigation />
-            <div>
-            {children}
-            </div>
-            </MetaMaskProvider>
-      </ReduxProvider>
+            <div>{children}</div>
+          </MetaMaskProvider>
+        </ReduxProvider>
       </body>
     </html>
   );
