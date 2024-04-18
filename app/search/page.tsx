f6e00623a400e6f09bf97fe68e2cb696b1e2de04
Martin Bozhilov <marogo142005@gmail.com>
2024-04-18 21:09:18 +0300

    [FIX] search design

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,48 +1,53 @@
-"use client"
+"use client";
 import { CONFIG } from "@/config/config";
-import algoliasearch from 'algoliasearch/lite';
-import { useEffect } from "react";
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch';
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, Hits } from "react-instantsearch";
 import ProductCard from "../stores/components/product";
-// import HitsGrid from "./components/hits";
-import './overwrite.css'
+import "./overwrite.css";
 
-const searchClient = algoliasearch(CONFIG.ALGOLIA_APP_ID, CONFIG.ALGOLIA_API_KEY)
+const searchClient = algoliasearch(
+  CONFIG.ALGOLIA_APP_ID,
+  CONFIG.ALGOLIA_API_KEY
+);
 
 const parseHit = (hit: any) => {
-    return {
-        ID: hit.objectID,
-        Name: hit.name,
-        Description: hit.description,
-        Price: hit.price,
-        ImageURL: hit.image_url ? hit.image_url : hit.imageURL
-    }
-}
+  return {
+    ID: hit.objectID,
+    Name: hit.name,
+    Description: hit.description,
+    Price: hit.price,
+    ImageURL: hit.image_url ? hit.image_url : hit.imageURL,
+  };
+};
 
-function Hit({ hit }: { hit: any}) {
-    return (
-      <ProductCard key={hit.ID} product={parseHit(hit)} />
-    );
-  }
-  
+function Hit({ hit }: { hit: any }) {
+  return <ProductCard key={hit.ID} product={parseHit(hit)} />;
+}
 
 const SearchPage = () => {
-    return (
-        <div className="flex w-full ">
-        <InstantSearch indexName="products" searchClient={searchClient}>
-        <SearchBox
-        placeholder="Search for products"
-        classNames={{
-          root: 'p-3 shadow-sm',
-          form: 'relative',
-          input: 'block w-full pl-9 pr-3 py-2 bg-white border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md focus:ring-1',
-          submitIcon: 'absolute top-0 left-0 bottom-0 w-6',
-        }}
-      />
-                <Hits hitComponent={Hit} className="flex w-full" />
-        </InstantSearch>
+  return (
+    <div className="flex flex-col w-full h-screen pt-24">
+      <InstantSearch indexName="products" searchClient={searchClient}>
+        <div className="flex-shrink-0 shadow-sm z-10 w-full ">
+          <SearchBox
+            placeholder="Search for products"
+            classNames={{
+              root: "flex w-full justify-center",
+              form: "flex w-full justify-center",
+              input:
+                " w-1/2 text-base rounded-lg block bg-transparent border-2 py-2.5 px-2",
+              submit: "hidden",
+            }}
+          />
         </div>
-    );
-}
+        <div className="flex-grow overflow-auto">
+          <div className="flex w-full justify-center">
+            <Hits hitComponent={Hit} className="flex w-full" />
+          </div>
+        </div>
+      </InstantSearch>
+    </div>
+  );
+};
 
 export default SearchPage;
